89ee856258ad01e599027c0c2e617ac7f9cb3e70
mj007 <mj007@mj007-ThinkPad-X220-Tablet>
2013-03-25 17:04:56 -0400

    Adding MyMalloc.h header file

---
+++ MyMalloc.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   MyMalloc.h
+ * Author: Amjad Al-Rikabi
+ * Student ID: 260143211
+ *
+ * Created on March 25, 2013, 4:37 PM
+ */
+
+#ifndef MYMALLOC_H
+#define	MYMALLOC_H
+
+void * my_malloc(int size);
+extern char *my_malloc_error;
+void my_free(void *ptr);
+void my_mallopt(int policy);
+void my_mallinfo();
+
+#endif	/* MYMALLOC_H */
+
