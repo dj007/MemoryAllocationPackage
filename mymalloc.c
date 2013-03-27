9f3188500e93ae0e092d683eff096f0ada980a3a
mj007 <mj007@mj007-ThinkPad-X220-Tablet>
2013-03-27 11:29:24 -0400

    Adding MyMalloc.c & pa2_test.c source files

---
+++ mymalloc.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   mymalloc.c
+ * Author: Amjad Al-Rikabi
+ * Student ID: 260143211
+ *
+ * Created on March 27, 2013, 11:20 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mymalloc.h"
+
+
+
